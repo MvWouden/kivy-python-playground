366d673f5446fcfddaedf66d14a4becab4f1a5da
Milan van Wouden <milanvanwouden@live.nl>
2023-07-13 22:39:38 +0200

    rewrite kivy builder loading

--- src/kivy_playground/pong/main.py
+++ src/kivy_playground/pong/main.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 from pathlib import Path
 
@@ -12,7 +13,18 @@ from src.kivy_playground.pong.widgets.ball.pong_ball import (  # noqa: F401
 from src.kivy_playground.pong.widgets.game.pong_game import PongGame
 
 kivy.require("2.2.1")
+
 DIR_PATH: str = os.path.dirname(os.path.realpath(__file__))
+WIDGETS: tuple[tuple[str, str], ...] = (
+    (
+        "src.kivy_playground.pong.widgets.game",
+        str(Path(DIR_PATH) / "widgets" / "game" / "pong_game.kv"),
+    ),
+    (
+        "src.kivy_playground.pong.widgets.ball",
+        str(Path(DIR_PATH) / "widgets" / "ball" / "pong_ball.kv"),
+    ),
+)
 
 
 class PongApp(App):
@@ -20,14 +32,14 @@ class PongApp(App):
 
     def build(self) -> Widget:
         """Build the pong application and register files."""
-        Builder.load_file(
-            str(Path(DIR_PATH) / "widgets" / "game" / "pong_game.kv")
-        )
-        Builder.load_file(
-            str(Path(DIR_PATH) / "widgets" / "ball" / "pong_ball.kv")
-        )
+        # Load Widgets dependencies
+        for widget, widget_kv in WIDGETS:
+            importlib.import_module(widget)
+            Builder.load_file(widget_kv)
 
-        return PongGame()
+        game = PongGame()
+
+        return game
 
 
 def run_pong_app() -> None:
---
+++ src/kivy_playground/pong/widgets/ball/__init__.py
@@ -0,0 +1,1 @@
+from .pong_ball import PongBall  # noqa: F401
